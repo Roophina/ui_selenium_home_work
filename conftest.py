69c6cf525790bcd5eee1fb03b49455c664358501
Rufina Shayikhieva <expelliarmus2012@yandex.ru>
2021-09-13 21:45:50 +0300

    Add change in conftest

--- conftest.py
+++ conftest.py
@@ -4,6 +4,10 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 from application.App import App
+from common.constants import Constants
+from locators.locators_login_page import LocatorsLoginPage
+from locators.locators_main_page import LocatorsMainPage
+from locators.locators_personal_account import LocatorsPersonalAccount
 
 
 def pytest_addoption(parser):
@@ -28,3 +32,19 @@ def app_fixture(request):
     )
     yield app
     app.quit()
+
+
+@pytest.fixture
+def authorization_fixture(app_fixture):
+    app_fixture.open_page(Constants.MAIN_PAGE)
+    app_fixture.main_page.click_on_link(LocatorsMainPage.LINK_ENTER)
+    login = app_fixture.login_page.find_element(LocatorsLoginPage.LOGIN)
+    app_fixture.login_page.fill_element(login, Constants.VALID_LOGIN)
+    password = app_fixture.login_page.find_element(LocatorsLoginPage.PASSWORD)
+    app_fixture.login_page.fill_element(password, Constants.VALID_PASSWORD)
+    app_fixture.login_page.click_on_button(LocatorsLoginPage.BUTTON_ENTER)
+    assert app_fixture.personal_account.find_element(
+        LocatorsPersonalAccount.PERSONAL_ACCOUNT
+    ), "couldn't log in with valid data"
+    yield
+    app_fixture.personal_account.exit_from_account()
