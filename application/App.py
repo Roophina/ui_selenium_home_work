5a1a211af828d6f6472f54f847aff70ea3d27614
Rufina Shayikhieva <expelliarmus2012@yandex.ru>
2021-09-13 21:44:16 +0300

    Add change in application

--- application/App.py
+++ application/App.py
@@ -1,6 +1,8 @@
+from pages.EditInformationPage import EditInformationPage
 from pages.LoginPage import LoginPage
 from pages.MainPage import MainPage
 from pages.PersonalAccountPage import PersonalAccountPage
+from pages.PreferencesPage import PreferencesPage
 
 
 class App:
@@ -9,6 +11,8 @@ class App:
         self.main_page = MainPage(self)
         self.login_page = LoginPage(self)
         self.personal_account = PersonalAccountPage(self)
+        self.preferences = PreferencesPage(self)
+        self.edit_information = EditInformationPage(self)
 
     def open_page(self, url):
         self.driver.get(url)
