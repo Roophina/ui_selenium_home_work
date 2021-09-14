39c1de2554450de196d4e33d74eca76f93c7fec8
Rufina Shayikhieva <expelliarmus2012@yandex.ru>
2021-09-14 18:36:45 +0300

    Add change in application

--- application/App.py
+++ application/App.py
@@ -1,3 +1,7 @@
+from pages.AdminPage import AdminPage
+from pages.CourseDeletePage import CourseDeletePage
+from pages.CourseEditPage import CourseEditPage
+from pages.CourseManagementPage import CourseManagementPage
 from pages.EditInformationPage import EditInformationPage
 from pages.LoginPage import LoginPage
 from pages.MainPage import MainPage
@@ -13,6 +17,10 @@ class App:
         self.personal_account = PersonalAccountPage(self)
         self.preferences = PreferencesPage(self)
         self.edit_information = EditInformationPage(self)
+        self.admin_page = AdminPage(self)
+        self.course_management = CourseManagementPage(self)
+        self.course_edit = CourseEditPage(self)
+        self.course_delete = CourseDeletePage(self)
 
     def open_page(self, url):
         self.driver.get(url)
