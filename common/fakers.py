4a00428925ac3659fa54ad365fb235f4680df146
Rufina Shayikhieva <expelliarmus2012@yandex.ru>
2021-09-14 18:38:06 +0300

    Add changes in fakers

--- common/fakers.py
+++ common/fakers.py
@@ -11,3 +11,15 @@ class BasicData:
     def random():
         city = fake.city_name()
         return BasicData(city)
+
+
+class NewCourse:
+    def __init__(self, full_name=None, short_name=None):
+        self.full_name = full_name
+        self.short_name = short_name
+
+    @staticmethod
+    def random():
+        full_name = fake.text(max_nb_chars=40)
+        short_name = fake.text(max_nb_chars=20)
+        return NewCourse(full_name, short_name)
